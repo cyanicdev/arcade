c16b12bdba31feca1fd909fe53dcf99178928b43
javadhamidi <19621911+javadhamidi@users.noreply.github.com>
2019-12-10 13:20:38 +1100

    well at least there's a player

--- nag1/game.js
+++ nag1/game.js
@@ -6,7 +6,7 @@ let config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: {y: 500},
+            gravity: { y: 500 },
             debug: true
         }
     },
@@ -19,14 +19,16 @@ let config = {
 };
 
 let game = new Phaser.Game(config);
- 
+
 function preload() {
-    this.load.image('player', 'assets/player.png');
+    this.load.spritesheet('player', 
+        'assets/player.png',
+        { frameWidth: 32, frameHeight: 32 }
+    );
 
     this.load.image('spritesheet', 'assets/spritesheet.png');
-    this.load.tilemapCSV("level", "assets/sample.csv");
 }
- 
+
 function create() {
     const sample = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -50,12 +52,52 @@ function create() {
         [1, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     ];
-    
-      const map = this.make.tilemap({ data: sample, tileWidth: 32, tileHeight: 32 });
+
+    const map = this.make.tilemap({ data: sample, tileWidth: 32, tileHeight: 32 });
     const tiles = map.addTilesetImage('spritesheet');
     const layer = map.createStaticLayer(0, tiles, 0, 0);
+
+    player = this.physics.add.sprite(100, 450, 'player');
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('player', { start: 0, end: 7 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    
+    this.anims.create({
+        key: 'idle',
+        frames: [ { key: 'player', frame: 8 } ],
+        frameRate: 10
+    });
+    
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('player', { start: 9, end: 16 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    cursors = this.input.keyboard.createCursorKeys();
 }
- 
+
 function update(time, delta) {
- 
+    if (cursors.left.isDown) {
+        player.setVelocityX(-160);
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+        player.anims.play('right', true);
+    }
+    else {
+        player.setVelocityX(0);
+        player.anims.play('idle');
+    }
+
+    if (cursors.up.isDown /*&& player.body.touching.down*/) {
+        player.setVelocityY(-330);
+    }
 }
